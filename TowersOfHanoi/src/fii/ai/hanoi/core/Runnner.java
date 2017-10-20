68cb06bb117b5014f94d88d9a723f4f2c17a0e9b
CristiHuma96 <cristihuma96@gmail.com>
2017-10-20 08:37:00 +0300

    removed intellij files

--- TowersOfHanoi/src/fii/ai/hanoi/core/Runnner.java
+++ TowersOfHanoi/src/fii/ai/hanoi/core/Runnner.java
@@ -1,7 +1,6 @@
 package fii.ai.hanoi.core;
 
-import com.sun.org.apache.regexp.internal.RE;
-
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,26 +8,35 @@ import java.util.List;
  * TODO rename this class
  */
 public class Runnner {
-    private List<Results> resultsList;
+    private List<Results> resultsList = new ArrayList<>();
 
     public static void main(String[] args) {
         new Runnner().run();
     }
 
-    private void run() {
-        Brain brain = null;
-        int rodCount = 0;
-        int discCount = 0;
+    private void run()
+    {
+        Brain brain = new RandomBrain();
+        int rodCount = 3;
+        int discCount = 3;
         int iterations = 1;
         SolutionFinder solutionFinder = new SolutionFinder(brain, rodCount, discCount);
-        for (int i = 0; i < iterations; i++) {
+        for (int i = 0; i < iterations; i++)
+        {
             solutionFinder.reset();
             Results results = solutionFinder.findSolution();
+            printSolutionDetailed(results.solution);
             resultsList.add(results);
         }
-
-
+        ResultsProcessorImpl resultsProcessor = new ResultsProcessorImpl();
+        resultsProcessor.processResults(resultsList);
     }
+        private void printSolutionDetailed(List<State> solution){
+            for (State state:solution){
+                System.out.println(state);
+            }
+    }
+
 
     private Runnner() {
 
