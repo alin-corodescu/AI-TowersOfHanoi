24d4577bca65be728b89fc93886ba09dbe871eb5
alin-corodescu <alin.corodescu@gmail.com>
2017-10-20 05:54:10 +0100

    runner implementation

--- TowersOfHanoi/src/fii/ai/hanoi/core/Runnner.java
+++ TowersOfHanoi/src/fii/ai/hanoi/core/Runnner.java
@@ -2,6 +2,8 @@ package fii.ai.hanoi.core;
 
 import com.sun.org.apache.regexp.internal.RE;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -9,25 +11,32 @@ import java.util.List;
  * TODO rename this class
  */
 public class Runnner {
-    private List<Results> resultsList;
+    private List<Results> resultsList = new ArrayList<>();
 
     public static void main(String[] args) {
         new Runnner().run();
     }
 
     private void run() {
-        Brain brain = null;
-        int rodCount = 0;
-        int discCount = 0;
+        Brain brain = new BacktrackingBrain();
+        int rodCount = 5;
+        int discCount = 5;
         int iterations = 1;
         SolutionFinder solutionFinder = new SolutionFinder(brain, rodCount, discCount);
         for (int i = 0; i < iterations; i++) {
             solutionFinder.reset();
             Results results = solutionFinder.findSolution();
+            printSolutionDetailed(results.solution);
             resultsList.add(results);
         }
 
 
+    }
+
+    private void printSolutionDetailed(List<State> solution) {
+        for (State state : solution) {
+            System.out.println(state + "->");
+        }
     }
 
     private Runnner() {
